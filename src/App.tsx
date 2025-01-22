33430aa1e48a8b520ebbd5cbe9734815253fb2a9
Tomasz <t.golab06@gmail.com>
2025-01-22 01:39:48 +0100

    Refactor App component to use styled-components, implement theme switching, and remove unused tab-switcher styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
-import styles from './App.module.scss'
+import styled, { ThemeProvider } from 'styled-components'
+import { Tabs } from './components/Tabs'
+import { ThemeSwitcher } from './components/ThemeSwitcher'
+import { Paragraph } from './components/Paragraph'
+import { useState } from 'react'
 
 function App() {
+    const [theme, setTheme] = useState<'light' | 'dark'>('light')
+
+    const light = {
+        colors: {
+            primary: '#5f6d6d',
+            textPrimary: 'white',
+            background: '#f9f9f9',
+            textBackground: 'black',
+        },
+    }
+
+    const dark = {
+        colors: {
+            primary: '#bfbfbf',
+            textPrimary: '#000000',
+            background: '#1a1a1a',
+            textBackground: '#bfbfbf',
+        },
+    }
+
+    const StyledWrapper = styled.div`
+        background-color: ${(props) => props.theme.colors.background};
+        height: 100vh;
+        juisify-content: center;
+    `
+
+    const StyledRow = styled.div`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        margin: 2rem 0;
+    `
+
     return (
-        <div className={styles['tab-switcher']}>
-            <div>
-                <button>
-                    <h2>Baza graczy</h2>
-                </button>
-            </div>
+        <ThemeProvider theme={theme === 'light' ? light : dark}>
+            <StyledWrapper>
+                <StyledRow>
+                    <Paragraph>Football App</Paragraph>
 
-            <div>
-                <button>
-                    <h2>Baza drużyn</h2>
-                </button>
-            </div>
+                    <ThemeSwitcher theme={theme} setTheme={setTheme} />
+                </StyledRow>
 
-            <div>
-                <button>
-                    <h2>Baza rozgrywek</h2>
-                </button>
-            </div>
-
-            <div>
-                <button>
-                    <h2>Statystyki</h2>
-                </button>
-            </div>
-        </div>
+                <Tabs />
+            </StyledWrapper>
+        </ThemeProvider>
     )
 }
 
