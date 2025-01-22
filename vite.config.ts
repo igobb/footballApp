758343f4eb3fe50543d6b1ed08f1938f57724f40
Tomasz <t.golab06@gmail.com>
2025-01-22 01:36:28 +0100

    Format Vite configuration for better readability

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react({
-        plugins: [[
-            '@swc/plugin-styled-components', {
-            displayName: true,
-        }
-        ]],
-    })],
+    plugins: [
+        react({
+            plugins: [
+                [
+                    '@swc/plugin-styled-components',
+                    {
+                        displayName: true,
+                    },
+                ],
+            ],
+        }),
+    ],
 })
