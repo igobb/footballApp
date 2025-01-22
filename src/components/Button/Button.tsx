253d2ea17c89f473af03fea2c790ae8b2915cd0a
Tomasz <t.golab06@gmail.com>
2025-01-22 01:39:32 +0100

    Add Button, Paragraph, and Tabs components with styled-components

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+interface ButtonProps {
+    label: string
+    onClick?: () => void
+}
+
+const StyledButton = styled.button`
+    color: ${(props) => props.theme.colors.textPrimary};
+    background-color: ${(props) => props.theme.colors.primary};
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+`
+
+export const Button = ({ label, onClick }: ButtonProps) => {
+    return <StyledButton onClick={onClick}>{label}</StyledButton>
+}
